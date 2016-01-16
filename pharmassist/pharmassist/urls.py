5de71f60e5c418a5102fbf12fd6053a7631a0aa5
vivianakinyi <vivian.akinyi@savannahinformatics.com>
2016-01-16 19:12:58 +0300

    Changed the admin url

--- pharmassist/pharmassist/urls.py
+++ pharmassist/pharmassist/urls.py
@@ -14,12 +14,7 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-# from django.conf.urls import url
-# from django.contrib import admin
 
-# urlpatterns = [
-#     url(r'^admin/', admin.site.urls),
-# ]
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
